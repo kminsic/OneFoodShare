f636c223ee6a7dd0d59851d1f040c327df8cd812
kimdoyoon <ehdns9800@gmail.com>
2023-04-11 19:13:14 +0900

    Entity와 연관관계 맵핑, jwt-refresh토큰 발급 하기전에 로그인 과정

---
+++ src/test/java/com/itsme/onefoodshare/OneFoodShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsme.onefoodshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneFoodShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
